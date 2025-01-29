32150453089a14da697c0527057242151f5f7aa5
oscar-hlustik-tnei <oscar.hlustik@tneigroup.com>
2025-01-29 15:49:11 +0000

    Added a unit test for demand = generation, Found an import point where demand != generation

--- tests.py
+++ tests.py
@@ -31,11 +31,45 @@ class TestRunBasicExample(unittest.TestCase):
         with open('.test_cache.p', 'wb') as f:
             pickle.dump(active_power, f)
 
+
         # Also add with statement for the read file to remove Unclosed file error
         with open('.test_cache.p', 'rb') as f:
             comparison_data = pickle.load(f)
 
         self.assertTrue(active_power.equals(comparison_data))
+
+    def test_constraints(self):
+        decision_fig, cumulative_cost_fig, active_power_fig, subplot_fig, active_power, c = run_basic_example(
+            t=config.T,
+            n=config.N,
+            restart_targets=config.RESTART_TARGETS,
+            block_limit=config.BLOCK_LIMIT,
+            generators_inactive=0,
+            min_operating_capacity=0.15
+        )
+
+
+        idx = active_power.index[2] #This is the time stamps in the data
+                                    #The wind generation never goes to 0
+                                    #At the first few time stamps generation > demand
+
+        demand = active_power.loc[idx, 'block demand']
+
+
+        generator_0= active_power.loc[idx, 'Generator 0']
+        generator_1= active_power.loc[idx, 'Generator 1']
+        generator_2= active_power.loc[idx, 'Generator 2']
+        generator_3= active_power.loc[idx, 'Generator 3']
+        generator_4= active_power.loc[idx, 'Generator 4']
+        solar = active_power.loc[idx, 'solar']
+        wind = active_power.loc[idx, 'wind']
+
+
+        gross_generation = solar + wind + generator_0 + generator_1 + generator_2 + generator_3 + generator_4
+
+        self.assertAlmostEqual(gross_generation, demand)
+
+
 
 if __name__ == '__main__':
     warnings.simplefilter('always', FutureWarning)
